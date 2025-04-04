3611fe7afb9cb0ebb6c483c472c7b9785f5de977
skeashly <sarah@keashly.com>
2025-04-03 17:11:48 -0700

    Update application_helper to show required label. Update new form with application_helper. Update validations to note that everything except for notes presence is required so the new helper method shows on the form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def required_label(form, field)
+    label_text = form.label(field)
+    if form.object.class.validators_on(field).any? { |v| v.is_a?(ActiveModel::Validations::PresenceValidator) }
+      label_text + " *"
+    else
+      label_text
+    end
+  end
 end
+
--- app/models/macro_entry.rb
+++ app/models/macro_entry.rb
@@ -2,7 +2,6 @@ class MacroEntry < ApplicationRecord
   belongs_to :user
 
   #validations
-  validates :protein, :fat, :carbs, :step_count, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
-  validates :date, presence: true
+  validates :protein, :fat, :carbs, :step_count, :date, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :notes, length: { maximum: 500 }, allow_blank: true
 end
